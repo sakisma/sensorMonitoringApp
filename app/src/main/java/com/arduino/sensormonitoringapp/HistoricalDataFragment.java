e5c48dbe9ac3e6de214fc873ccc710a37b4a2511
savva <savvassimeonidis@gmail.com>
2024-11-03 18:47:54 +0200

    HeatMapChart library

--- app/src/main/java/com/arduino/sensormonitoringapp/HistoricalDataFragment.java
+++ app/src/main/java/com/arduino/sensormonitoringapp/HistoricalDataFragment.java
@@ -1,123 +1,124 @@
 package com.arduino.sensormonitoringapp;
 
-import android.graphics.Color;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Spinner;
+import android.view.MenuItem;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
-import com.github.mikephil.charting.charts.BarChart;
-import com.github.mikephil.charting.charts.LineChart;
-import com.github.mikephil.charting.components.XAxis;
-import com.github.mikephil.charting.data.Entry;
-import com.github.mikephil.charting.data.LineData;
-import com.github.mikephil.charting.data.LineDataSet;
-import com.github.mikephil.charting.formatter.ValueFormatter;
-import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-import java.util.Locale;
-
-public class HistoricalDataFragment extends Fragment {
-    private LineChart lineChart;
-//    private HeatMapChart heatMapChart;
-    private BarChart barChart;
+public class MainActivity extends AppCompatActivity implements NavigationBarView.OnItemSelectedListener {
     private FirebaseDatabase database;
     private DatabaseReference sensorDataRef;
-    private Spinner dateRangeSpinner;
-    @Nullable
+    private BottomNavigationView bottomNavigationView;
+
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_historical_data, container, false);
-        lineChart = view.findViewById(R.id.line_chart);
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Bottom navigation
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnItemSelectedListener(this);
+        loadFragment(new HomeFragment());
+
+        // Initialize Firebase Database
         database = FirebaseDatabase.getInstance();
         sensorDataRef = database.getReference("sensorData");
 
-        fetchHistoricalData();
-
-        return view;
+        // Fetch latest data and update the UI
+        fetchLatestData();
     }
 
-    private void fetchHistoricalData() {
-        sensorDataRef.addValueEventListener(new ValueEventListener() {
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        Fragment fragment;
+        int id = item.getItemId();
+
+        if (id == R.id.navigation_home) {
+            fragment = new HomeFragment();
+        } else if (id == R.id.navigation_historical_data) {
+            fragment = new HistoricalDataFragment();
+        } else {
+            return false;
+        }
+        loadFragment(fragment);
+        return true;
+    }
+
+    private void fetchLatestData() {
+        // Query to get the latest date entry from Firebase
+        sensorDataRef.limitToLast(1).addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                List<Entry> entries = new ArrayList<>();
-
                 for (DataSnapshot dateSnapshot : dataSnapshot.getChildren()) {
-                    String date = dateSnapshot.getKey(); // Get the date (e.g., "2024-09-26")
-                    for (DataSnapshot timeSnapshot : dateSnapshot.getChildren()) {
-                        String time = timeSnapshot.getKey(); // Get the time (e.g., "01:38:03")
+                    String latestDate = dateSnapshot.getKey();  // Get the latest date (e.g., "2024-09-26")
 
-                        // Combine date and time to create a timestamp string
-                        String timestampString = date + " " + time;
+                    // Query to get the latest time entry under the latest date
+                    DatabaseReference timeRef = sensorDataRef.child(latestDate);
+                    timeRef.limitToLast(1).addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot timeSnapshot) {
+                            for (DataSnapshot timeEntry : timeSnapshot.getChildren()) {
+                                String latestTime = timeEntry.getKey();  // Get the latest time (e.g., "01:38:03")
 
-                        // Parse the timestamp string into a Date object
-                        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault());
-                        try {
-                            Date timestampDate = dateFormat.parse(timestampString);
-                            if (timestampDate != null) {
-                                long timestamp = timestampDate.getTime(); // Get the timestamp in milliseconds
+                                // Fetch the latest temperature and moisture values
+                                Object tempObject = timeEntry.child("temp").getValue();
+                                Object moistureObject = timeEntry.child("moisture").getValue();
 
-                                Object tempObject = timeSnapshot.child("temp").getValue();
-                                if (tempObject != null) {
-                                    float tempValue = Float.parseFloat(tempObject.toString());
+                                // Check if temp and moisture values are not null
+                                if (tempObject != null && moistureObject != null) {
+                                    String latestTemp = tempObject.toString();
+                                    String latestMoisture = moistureObject.toString();
 
-                                    entries.add(new Entry(timestamp, tempValue));
+                                    // Update the UI with the latest values
+                                    TextView tempValueText = findViewById(R.id.tempValue);
+                                    TextView moistureValueText = findViewById(R.id.moistureValue);
+
+                                    tempValueText.setText(latestTemp + " °C");
+                                    moistureValueText.setText(latestMoisture + " %");
+                                } else {
+                                    // Handle missing or null values here (optional)
+                                    TextView tempValueText = findViewById(R.id.tempValue);
+                                    TextView moistureValueText = findViewById(R.id.moistureValue);
+
+                                    tempValueText.setText("-- °C");
+                                    moistureValueText.setText("-- %");
                                 }
                             }
-                        } catch (ParseException e) {
-                            // Handle parsing exception
-                            e.printStackTrace();
                         }
-                    }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
+                            // Handle database error
+                        }
+                    });
                 }
-
-                LineDataSet dataSet = new LineDataSet(entries, "Temperature");
-                dataSet.setColor(Color.RED);
-                dataSet.setValueTextColor(Color.BLACK);
-
-                List<ILineDataSet> dataSets = new ArrayList<>();
-                dataSets.add(dataSet);
-
-                LineData lineData = new LineData(dataSets);
-
-                lineChart.setData(lineData);
-
-
-                // Formatting the x-axis to show dates
-                XAxis xAxis = lineChart.getXAxis();
-                xAxis.setValueFormatter(new ValueFormatter() {
-                    @Override
-                    public String getFormattedValue(float value)
-                    {
-                        Date date = new Date((long) value);
-                        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM HH:mm", Locale.getDefault());
-                        return sdf.format(date);
-                    }
-                });
-
-                lineChart.invalidate(); // Refresh the chart
             }
 
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
-                // Handle error
+                // Handle database error
             }
         });
+    }
+
+
+    private void loadFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.content_frame,fragment);
+        transaction.commit();
     }
 }
